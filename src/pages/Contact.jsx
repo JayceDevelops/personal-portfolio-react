79f9c849e31b85a90f48d3f493e12d03d0657609
JayceDevelops <jayceiezzi@gmail.com>
2026-08-16 20:26:03 -0400

    Finished adding socials

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import Nav from "../components/Nav"
+import Socials from "../components/Socials"
 
 export default function Contact() {
     return (
         <div className="flex flex-col bg-background min-h-dvh">
             <Nav/>
+            <Socials/>
         </div>
     );
 }
